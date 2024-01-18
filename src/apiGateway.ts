0ad8fb0c5dee314f3819a6d2c8799b0c6028c309
César Andrés Correa Pacheco <cesar.correa@bci.cl>
2024-01-18 15:35:42 -0300

    fix: type checking

--- src/apiGateway.ts
+++ src/apiGateway.ts
@@ -3,6 +3,7 @@ import * as url from 'url';
 import { APIGatewayProxyEvent, APIGatewayProxyEventV2 } from 'aws-lambda/trigger/api-gateway-proxy';
 import QueryString from 'qs';
 
+
 export function httpRequestToEvent(request: Request, version?: string): APIGatewayProxyEvent | APIGatewayProxyEventV2 {
     const headers = objectMap(request.headers, (value): string | undefined => {
         if (Array.isArray(value)) {
@@ -38,44 +39,79 @@ export function httpRequestToEvent(request: Request, version?: string): APIGatew
     const shouldSendBase64 = request.method === 'GET' ? false : bodyString.includes('Content-Disposition: form-data');
 
     const cookies = request.headers.cookie ? request.headers.cookie.split('; ') : [];
-    if (version === "1") {
-        return {
-            version: '1.0',
-            resource: request.path,
-            path: request.path,
-            httpMethod: request.method,
-            headers: {
-                'x-forwarded-proto': request.protocol,
-                'x-forwarded-port': `${request.socket.localPort}`,
-                'x-forwarded-for': request.ip,
-                ...headers,
-            },
-            queryStringParameters,
-            requestContext: {
-                http: {
-                    method: request.method,
-                    path: request.path,
-                    protocol: request.protocol,
-                    sourceIp: request.ip,
-                    userAgent: request.header('User-Agent') ?? '',
+        if (version === "1") {
+            return {
+                version: '1.0',
+                resource: request.path,
+                path: request.path,
+                httpMethod: request.method,
+                headers: {
+                    'x-forwarded-proto': request.protocol,
+                    'x-forwarded-port': `${request.socket.localPort}`,
+                    'x-forwarded-for': request.ip,
+                    ...headers,
                 },
-                accountId: '123456789012',
-                apiId: 'api-id',
-                domainName: 'localhost',
-                domainPrefix: '',
-                requestId: 'id',
-                routeKey: '$default',
-                stage: '$default',
-                time: new Date().toISOString(),
-                timeEpoch: Date.now(),
-            },
-            pathParameters: {},
-            stageVariables: {},
-            body: shouldSendBase64 ? request.body.toString('base64') : bodyString,
-            isBase64Encoded: shouldSendBase64,
-            cookies: cookies,
-        };
-    }
+                queryStringParameters,
+                requestContext: {
+                    path: request.path,
+                    requestTimeEpoch: Date.now(),
+                    resourceId: 'resource-id',
+                    resourcePath: request.path,
+                    http: {
+                        method: request.method,
+                        path: request.path,
+                        protocol: request.protocol,
+                        sourceIp: request.ip || '',
+                        userAgent: request.header('User-Agent') ?? '',
+                    },
+                    accountId: '123456789012',
+                    apiId: 'api-id',
+                    domainName: 'localhost',
+                    domainPrefix: '',
+                    requestId: 'id',
+                    routeKey: '$default',
+                    stage: '$default',
+                    time: new Date().toISOString(),
+                    timeEpoch: Date.now(),
+                    authorizer: null,
+                    protocol: '',
+                    httpMethod: '',
+                    identity: {
+                        accessKey: null,
+                        accountId: null,
+                        caller: null,
+                        cognitoAuthenticationProvider: null,
+                        cognitoAuthenticationType: null,
+                        cognitoIdentityId: null,
+                        cognitoIdentityPoolId: null,
+                        principalOrgId: null,
+                        sourceIp: "192.0.2.1",
+                        user: null,
+                        userAgent: "user-agent",
+                        userArn: null,
+                        clientCert: {
+                          clientCertPem: "CERT_CONTENT",
+                          subjectDN: "www.example.com",
+                          issuerDN: "Example issuer",
+                          serialNumber: "a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1:a1",
+                          validity: {
+                            notBefore: "May 28 12:30:02 2019 GMT",
+                            notAfter: "Aug  5 09:36:04 2021 GMT"
+                          },
+                        },
+                        apiKey: null,
+                        apiKeyId: null,
+                    },
+                },
+                pathParameters: {},
+                stageVariables: {},
+                body: shouldSendBase64 ? request.body.toString('base64') : bodyString,
+                isBase64Encoded: shouldSendBase64,
+                cookies: cookies,
+                multiValueHeaders: {},
+                multiValueQueryStringParameters: {},
+            };
+        }
 
     return {
         version: '2.0',
@@ -99,7 +135,7 @@ export function httpRequestToEvent(request: Request, version?: string): APIGatew
                 method: request.method,
                 path: request.path,
                 protocol: request.protocol,
-                sourceIp: request.ip,
+                sourceIp: request.ip || '',
                 userAgent: request.header('User-Agent') ?? '',
             },
             accountId: '123456789012',
